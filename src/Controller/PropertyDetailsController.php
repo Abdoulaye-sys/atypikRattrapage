fe60cea8560dc4d850fd689b56d6d09b5ce4d296
abdoulaye-sys <abdoulayeba98@outlook.com>
2024-01-30 17:10:34 +0100

    adminPractilyDone

---
+++ src/Controller/PropertyDetailsController.php
@@ -0,0 +1,30 @@
+<?php
+// src/Controller/PropertyDetailsController.php
+
+namespace App\Controller;
+
+use App\Entity\Property;  // Assurez-vous d'importer l'entité Property
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
+
+class PropertyDetailsController extends AbstractController
+{
+    #[Route('/property/details/{id}', name: 'app_property_details')]
+    public function showDetails($id, PersistenceManagerRegistry $doctrine): Response
+    {
+        // Récupérer l'entité Property correspondante à l'ID
+        $property = $doctrine->getRepository(Property::class)->find($id);
+
+        // Vérifier si la propriété existe
+        if (!$property) {
+            throw $this->createNotFoundException('Propriété non trouvée');
+        }
+
+        // Rendre la vue avec les détails de la propriété
+        return $this->render('property_details/details.html.twig', [
+            'property' => $property,
+        ]);
+    }
+}
