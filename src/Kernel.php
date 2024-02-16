4643dc5d402637f67ad8d0a18ee908ac5df1ce11
abdoulaye-sys <abdoulayeba98@outlook.com>
2024-02-16 12:46:23 +0100

    procfile

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,47 +1,11 @@
 <?php
 
-// src/Kernel.php
-
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-use Symfony\Component\Routing\RouteCollectionBuilder;
-
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-
-    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
-    {
-        $container->setParameter('container.dumper.inline_class_loader', true);
-        $confDir = $this->getProjectDir().'/config';
-
-        $loader->load($confDir.'/{packages}/*.yaml');
-        $loader->load($confDir.'/{packages}/'.$this->environment.'/*.yaml');
-        $loader->load($confDir.'/{services}.yaml');
-    }
-
-    protected function configureRoutes(RouteCollectionBuilder $routes): void
-    {
-        $confDir = $this->getProjectDir().'/config';
-
-        $routes->import($confDir.'/{routes}/'.$this->environment.'/*.yaml');
-        $routes->import($confDir.'/{routes}/'.$this->environment.'/*.yaml');
-        $routes->import($confDir.'/{routes}/*.yaml');
-    }
-
-    public function registerBundles(): iterable
-    {
-        $contents = require $this->getProjectDir().'/config/bundles.php';
-        foreach ($contents as $class => $envs) {
-            if ($envs[$this->environment] ?? $envs['all'] ?? false) {
-                yield new $class();
-            }
-        }
-    }
 }
